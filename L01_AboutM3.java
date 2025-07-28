7899163e45b852e52e00af5c8d6c883ee317be1f
rsp-for-U <rohit.singh.parihar.2909@gmail.com>
2025-07-29 00:00:44 +0530

    Create L01_AboutM3.java
    Last moment completion!

---
+++ L01_AboutM3.java
@@ -0,0 +1,15 @@
+class L01_AboutM3{
+    public static void main (String[] args){
+      System.out.println("Name : Gajni");
+      System.out.println("email : xyz@blackrock.com");
+      System.out.println("Time : 23:55");
+      System.out.println("Dinner : had Dal-Roti");
+      System.out.println("Fav cricketer : VK18");
+      System.out.println("Car1 : i20 N-line");
+      System.out.println("Car2 : Safari");
+      System.out.println("Car3 : Mustang");
+      System.out.println("Destination : Shimla");
+    
+
+    }
+}
